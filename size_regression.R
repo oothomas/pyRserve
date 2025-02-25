2c2a82f8cdb5b42751260e75f7a02e47595e36b3
muratmaga <muratmaga@outlook.com>
2025-02-25 13:45:02 -0800

    Update size_regression.R

--- size_regression.R
+++ size_regression.R
@@ -2,16 +2,14 @@ library(SlicerMorphR)
 library(geomorph)
 #log=file.choose()
 
-log="./LMs/2023-12-29_10_41_10/analysis.log"
-parsed=parser(log)
+log="~/Desktop/2025-02-25_13_40_06/analysis.json"
+parsed=parser2(log)
 
 dat=read.csv(paste(parsed$output.path, parsed$OutputData, sep="/"))
-size=dat$centeroid
+size=dat$centroid
 coords=arrayspecs(dat[,4:ncol(dat)], p=parsed$no.LM, k=3)
 gdf=geomorph.data.frame(Size=size, Coords=coords)
 
 
 size.model = as.formula(Coords~Size)
 outlm=procD.lm(size.model, data=gdf)
-
-
